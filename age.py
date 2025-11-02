3ddce66c15f0b1e2e1d252e0d2f6618c9ff19cc8
CHRYSTAR <christakarendi@gmail.com>
2025-11-02 16:12:03 +0300

    age calculations

---
+++ age.py
@@ -0,0 +1,8 @@
+age= int(input("How old are you?"))
+months= age * 12
+days = age * 365
+weeks= age * 52
+print("You are",age,"years old")
+print("Months",months)
+print("Days",days)
+print("Weeks", weeks)
