62d9b64176001774e6075a401d1eea437be54d09
CHRYSTAR <christakarendi@gmail.com>
2025-11-02 22:02:50 +0300

    book_reading

---
+++ book_reading.py
@@ -0,0 +1,4 @@
+pagesinthebook = int(input("How many pages are in the book?"))
+pagesperday = int(input("How many pages do you read in a day?"))
+days = pagesinthebook * pagesperday
+print("It will take",days,"days")
