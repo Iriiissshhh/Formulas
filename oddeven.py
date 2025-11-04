bd46c5095a2a5620f1ddb264fe6c596958ad8026
CHRYSTAR <christakarendi@gmail.com>
2025-11-04 14:19:21 +0300

    odd_even

---
+++ oddeven.py
@@ -0,0 +1,12 @@
+number = int(input("Enter number: "))
+if number > 0:
+    print("Number is positive")
+if number < 0:
+    print("Number is negative")
+if number % 2==0:
+    print("Number is even")
+if number % 2==1:
+    print("Number is odd")
+else:
+    print("Zero")
+
