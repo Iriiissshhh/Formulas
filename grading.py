dc975d4d970c88374fe28693acb377ea7890044d
CHRYSTAR <christakarendi@gmail.com>
2025-11-04 13:34:00 +0300

    First grading

---
+++ grading.py
@@ -0,0 +1,11 @@
+score = int(input("What did you get?"))
+if score > 90:
+    print("You have an A")
+elif score > 80:
+    print("You have a B")
+elif score > 70:
+    print("You have a C")
+elif score > 60:
+    print("You have a D")
+else:
+    print("You have Failed")
