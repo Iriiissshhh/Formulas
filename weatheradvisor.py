05d975307fa966eee31a1e64284eec12dc77023b
CHRYSTAR <christakarendi@gmail.com>
2025-11-06 15:42:12 +0300

    weather advisor

---
+++ weatheradvisor.py
@@ -0,0 +1,11 @@
+temperature = float(input("Enter temperature:"))
+if temperature <0:
+    print(f"Freezing- stay inside")
+elif temperature <10:
+    print(f"Very cold - wear coat")
+elif temperature < 20:
+    print(f"Cool - jacket needed")
+elif temperature < 30:
+    print(f"Pleasant")
+else:
+    print("Hot - stay hydrated")
