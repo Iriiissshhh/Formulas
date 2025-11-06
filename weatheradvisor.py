03e6cf8e8abb5ca3454570182d806e6e9b75a264
CHRYSTAR <christakarendi@gmail.com>
2025-11-06 16:17:18 +0300

    second submission

--- weatheradvisor.py
+++ weatheradvisor.py
@@ -1,11 +1,16 @@
-temperature = float(input("Enter temperature:"))
-if temperature <0:
-    print(f"Freezing- stay inside")
-elif temperature <10:
-    print(f"Very cold - wear coat")
-elif temperature < 20:
-    print(f"Cool - jacket needed")
-elif temperature < 30:
-    print(f"Pleasant")
-else:
-    print("Hot - stay hydrated")
+while True:
+    temperature = float(input("Enter temperature:"))
+    if temperature <0:
+        print(f"Freezing- stay inside")
+    elif temperature <10:
+        print(f"Very cold - wear coat")
+    elif temperature < 20:
+        print(f"Cool - jacket needed")
+    elif temperature < 30:
+        print(f"Pleasant")
+    else:
+        print("Hot - stay hydrated")
+    again = input(f"Enter another temperature (yes or no)")
+    if again != "yes":
+        print("Be safe")
+        break 
